a108e500ee1aa266e4d0c17140ae7047ac1285e2
RayAra2004 <raynan.araujo@gmail.com>
2023-06-29 17:09:44 -0300

    Post Participants

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import dotenv from "dotenv";
 import express, { json } from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
+import joi from 'joi';
+import dayjs from "dayjs";
+import "dayjs/locale/pt-br.js";
 
 dotenv.config();
 
@@ -21,5 +24,39 @@ try{
 }
 
 const db = mongoClient.db();
+
+app.post('/participants', async (req, res) =>{
+    const { name } = req.body;
+
+    const userSchema = joi.object({
+        name: joi.string().required()
+    });
+    
+    const validation = userSchema.validate({name});
+    if(validation.error){
+        return res.sendStatus(422);
+    }
+
+    try{
+        const userExist = await db.collection('participants').findOne({name: name});
+        if(userExist) return res.sendStatus(409);
+        
+        db.collection('participants').insertOne({name: name, lastStatus: Date.now()});
+
+        const newMessage = {
+            from: name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: dayjs().locale("pt").format("H:mm:ss")
+        }
+
+        db.collection('messages').insertOne(newMessage);
+
+        res.sendStatus(201);
+    }catch(err){
+        return res.sendStatus(500);
+    }
+});
 
 app.listen(PORT, ()=> console.log(`Servidor rondando na porta ${PORT}`));
