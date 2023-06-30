aaad3c5b7fd2d3ca461780d870836f83da888554
RayAra2004 <raynan.araujo@gmail.com>
2023-06-30 09:49:32 -0300

    Post messages

--- src/app.js
+++ src/app.js
@@ -48,7 +48,7 @@ app.post('/participants', async (req, res) =>{
             to: 'Todos',
             text: 'entra na sala...',
             type: 'status',
-            time: dayjs().locale("pt").format("H:mm:ss")
+            time: dayjs().locale("pt").format("HH:mm:ss")
         }
 
         db.collection('messages').insertOne(newMessage);
@@ -67,6 +67,35 @@ app.get('/participants', async (req, res) => {
         res.sendStatus(500);
     }
     
+})
+
+app.post('/messages', async (req, res) => {
+    const { user } = req.headers;
+    const {to, text, type} = req.body;
+
+    const userSchema = joi.object({
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: joi.string().valid('message', 'private_message'),
+        from: joi.string().required()
+    });
+
+    const newMessage = {to, text, type, from: user};
+
+    try{
+        const userDB = await db.collection('participants').findOne({name: user})
+        const validation = userSchema.validate(newMessage);
+
+        if(validation.error || !userDB){
+            return res.sendStatus(422);
+        }
+        newMessage.time = dayjs().locale("pt").format("HH:mm:ss");
+        db.collection('messages').insertOne(newMessage);
+    } catch(err){
+        return res.sendStatus(500);
+    }
+
+    res.sendStatus(201);
 })
 
 app.listen(PORT, ()=> console.log(`Servidor rondando na porta ${PORT}`));
